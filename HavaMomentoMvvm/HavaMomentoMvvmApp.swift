718768444bdf7e67f7e0c6b043d89d534cc58397
Davletvm87 <d.ataballyev@yandex.com>
2021-05-19 00:54:18 +0300

    Initial Commit

---
+++ HavaMomentoMvvm/HavaMomentoMvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  HavaMomentoMvvmApp.swift
+//  HavaMomentoMvvm
+//
+//  Created by subje bilisim on 19.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct HavaMomentoMvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
